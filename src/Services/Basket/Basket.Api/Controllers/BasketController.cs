117dfa1f54e7f3789c5702a189b778bbb2a0e3ef
nobelrakib03 <nobelrakib>
2023-04-28 12:25:53 +0800

    discount grpc done

--- src/Services/Basket/Basket.Api/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using Basket.API.Entities;
-//using Basket.API.GrpcServices;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories.Interfaces;
 //using EventBus.Messages.Events;
 using MassTransit;
@@ -16,16 +16,16 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
-       // private readonly DiscountGrpcService _discountGrpcService;
-        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly DiscountGrpcService _discountGrpcService;
+      //  private readonly IPublishEndpoint _publishEndpoint;
         private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository repository, IMapper mapper)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService, IMapper mapper)
         {
             //DiscountGrpcService discountGrpcService
             //IPublishEndpoint publishEndpoint
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
-           // _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
            // _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
@@ -45,8 +45,8 @@ namespace Basket.API.Controllers
             // Communicate with Discount.Grpc and calculate lastest prices of products into sc
             foreach (var item in basket.Items)
             {
-              //  var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
-              //  item.Price -= coupon.Amount;
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
             }
 
             return Ok(await _repository.UpdateBasket(basket));
